43b528087967e2b263cef92118fa0f338cd24ad6
Yndari <sem-nikitka@mail.ru>
2023-07-17 18:37:53 +0600

    Classes

---
+++ OOP/classes.py
@@ -0,0 +1,18 @@
+class Auto:
+    def __init__(self, model, color, year): # инициализация
+        self.model = model  # атрибут класса
+        self.color = color # атрибут класса
+        self.year = year # атрибут класса
+
+    def display_info(self):
+        '''Методы для вывода данных'''
+        print(f'Model: {self.model}\nYear: {self.year}\nColor: {self.color}')
+
+
+auto1 = Auto('Lexus', 'black', 2004)
+auto2 = Auto('Toyota', 'black', 2015)
+auto1.display_info()
+auto2.display_info()
+print(auto1.model)
+
+
